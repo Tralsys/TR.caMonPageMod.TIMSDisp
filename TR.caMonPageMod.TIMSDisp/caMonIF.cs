d07a15010e5e12c422bee1bbdbab825d73a218cf
ＴＲ <public@technotter.com>
2020-11-02 01:18:10 +0900

    Create caMonIF.cs

---
+++ TR.caMonPageMod.TIMSDisp/caMonIF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+
+using caMon;
+
+namespace TR.caMonPageMod.TIMSDisp
+{
+	public class caMonIF : IPages
+	{
+		public Page FrontPage { get; } = new FrontPage();
+
+		public event EventHandler BackToHome;
+		public event EventHandler CloseApp;
+
+		public void Dispose()
+		{
+			
+		}
+	}
+}
