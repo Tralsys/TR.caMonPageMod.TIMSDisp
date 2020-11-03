bedf06e2aaaac472e14e9bea52e059ee7f3500d3
ＴＲ <public@technotter.com>
2020-11-04 02:26:20 +0900

    Update CommonMethods.cs
    不要なコメントを削除

--- TR.caMonPageMod.TIMSDisp/CommonMethods.cs
+++ TR.caMonPageMod.TIMSDisp/CommonMethods.cs
@@ -10,7 +10,6 @@ namespace TR.caMonPageMod.TIMSDisp
 {
 	static internal class CommonMethods
 	{
-		//ref : https://qiita.com/fujieda/items/d8642eae891d096d4028
 		static readonly WindowsMediaPlayer Btn_Pi;
 		static readonly int[] VolumeSettings = new int[3] { 0, 30, 60 };
 
@@ -24,10 +23,8 @@ namespace TR.caMonPageMod.TIMSDisp
 		static CommonMethods()
 		{
 			//ref : http://dobon.net/vb/dotnet/programing/playmidifile.html#wmp
-			//Btn_Pi = Activator.CreateInstance(Type.GetTypeFromProgID("WMPlayer.OCX.7"));
 			Btn_Pi = new WindowsMediaPlayer();
 			Btn_Pi.URL = Path.Combine(CurrentDLLPath, "_Resources", "Sounds", "Btn_Pi.wav");
-			//Btn_Pi.URL = @"pack://application:,,,/TR.caMonPageMod.TIMSDisp;component/_Resources/Sounds/Btn_Pi.wav";
 		}
 
 		static public void ButtonPushed()
@@ -40,7 +37,6 @@ namespace TR.caMonPageMod.TIMSDisp
 				Btn_Pi.controls.stop();
 
 			Btn_Pi.settings.volume = CurrentVolumeSetting;
-			//Btn_Pi.controls.currentPosition = 0;
 			Btn_Pi.controls.play();
 		}
 		
