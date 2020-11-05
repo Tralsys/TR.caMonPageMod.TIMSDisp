38151b55dc9d69a5ea3a275d869910f644457867
ＴＲ <public@technotter.com>
2020-11-06 01:21:41 +0900

    Create UsefulFuncs.cs
    エンコードの問題を確実に解決するため, caMonIFからエンコード設定読み込み命令を移動

---
+++ TR.caMonPageMod.TIMSDisp/_UsefulFuncs/UsefulFuncs.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace TR.caMonPageMod.TIMSDisp._UsefulFuncs
+{
+	public static partial class UsefulFuncs
+	{
+		static UsefulFuncs()
+		{
+			//ref : https://aquasoftware.net/blog/?p=895
+			//エンコード932(Shift-JIS)に対応するため
+			//Char_WideNarrowがらみ
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+	}
+}
