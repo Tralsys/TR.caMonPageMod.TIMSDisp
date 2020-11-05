86ce17fecc79fec2c171631e684083adced7688b
ＴＲ <public@technotter.com>
2020-11-05 09:25:03 +0900

    Create Char_WideNarrow.cs
    全角半角変換用メソッドを追加

---
+++ TR.caMonPageMod.TIMSDisp/_UsefulFuncs/Char_WideNarrow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+using Microsoft.VisualBasic;
+
+namespace TR.caMonPageMod.TIMSDisp._UsefulFuncs
+{
+	public static partial class UsefulFuncs
+	{
+		public static object WideNarrowConv(DependencyObject d, object baseValue)
+		{
+			if (d is IChar_WideNarrow wn && baseValue is string s)
+				try {
+					return Strings.StrConv(s, wn.IsHankaku ? VbStrConv.Narrow : VbStrConv.Wide, 0x411);
+				}catch(Exception e)
+				{
+					Debug.WriteLine("Str:{0}\n{1}", s, e);
+					throw;
+				}
+			else
+				throw new ArgumentException("Entered DependencyObject is not IChar_WideNarrow, or baseValue is not string");
+		}
+
+	}
+
+	public interface IChar_WideNarrow
+	{
+		public bool IsHankaku { get; }
+	}
+}
