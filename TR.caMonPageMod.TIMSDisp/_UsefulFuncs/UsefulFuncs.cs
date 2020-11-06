d4e8ce6994a3c33865bd51bef68bf382c3711567
ＴＲ <public@technotter.com>
2020-11-07 01:27:40 +0900

    Update UsefulFuncs.cs
    GetPixelsを追加

--- TR.caMonPageMod.TIMSDisp/_UsefulFuncs/UsefulFuncs.cs
+++ TR.caMonPageMod.TIMSDisp/_UsefulFuncs/UsefulFuncs.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Windows.Media;
 
 namespace TR.caMonPageMod.TIMSDisp._UsefulFuncs
 {
@@ -10,6 +12,25 @@ namespace TR.caMonPageMod.TIMSDisp._UsefulFuncs
 			//エンコード932(Shift-JIS)に対応するため
 			//Char_WideNarrowがらみ
 			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+
+		static public byte[] GetPixels(Brush b, byte[] arr = null) => GetPixels(b as SolidColorBrush, arr);
+		static public byte[] GetPixels(SolidColorBrush b, byte[] arr = null)
+		{
+			if (b == null)
+				return null;
+
+			arr ??= new byte[4];
+			if (arr.Length < 4)
+				arr = new byte[4];
+
+			int i = 0;
+			arr[i++] = b.Color.B;
+			arr[i++] = b.Color.G;
+			arr[i++] = b.Color.R;
+			arr[i++] = b.Color.A;
+
+			return arr;
 		}
 	}
 }
