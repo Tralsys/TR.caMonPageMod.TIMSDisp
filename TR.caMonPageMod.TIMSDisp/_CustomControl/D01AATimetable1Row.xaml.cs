7121f865c6181abfb6ecba760e7e8eebbfb2a672
ＴＲ <public@technotter.com>
2020-11-07 21:49:55 +0900

    D01AATimetable1Rowを追加
    とりあえずレイアウトだけ

---
+++ TR.caMonPageMod.TIMSDisp/_CustomControl/D01AATimetable1Row.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TR.caMonPageMod.TIMSDisp._CustomControl
+{
+	public class D01AATimetable1Row : Control
+	{
+
+		static D01AATimetable1Row()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(D01AATimetable1Row), new FrameworkPropertyMetadata(typeof(D01AATimetable1Row)));
+		}
+	}
+}
