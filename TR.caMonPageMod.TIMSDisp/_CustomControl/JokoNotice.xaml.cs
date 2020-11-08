d9230f62cd6cc8a829898759720f6d6c174e1f9c
ＴＲ <public@technotter.com>
2020-11-08 19:07:52 +0900

    徐行通知のレイアウト実装

---
+++ TR.caMonPageMod.TIMSDisp/_CustomControl/JokoNotice.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TR.caMonPageMod.TIMSDisp._CustomControl
+{
+	public class JokoNotice : Control
+	{
+		static JokoNotice() => DefaultStyleKeyProperty.OverrideMetadata(typeof(JokoNotice), new FrameworkPropertyMetadata(typeof(JokoNotice)));
+		
+	}
+}
