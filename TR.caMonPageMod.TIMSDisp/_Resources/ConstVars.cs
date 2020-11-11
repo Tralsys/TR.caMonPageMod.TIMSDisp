e5ba2ba0ee0e4e1e5ea1e5512227dd3b7875e4c7
ＴＲ <public@technotter.com>
2020-11-12 01:47:21 +0900

    Create ConstVars.cs
    定数値を記述するクラス

---
+++ TR.caMonPageMod.TIMSDisp/_Resources/ConstVars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace TR.caMonPageMod.TIMSDisp._Resources
+{
+	static public class ConstVars
+	{
+		static public readonly Brush STATION_DEFAULT_TEXTCOLOR = Brushes.White;
+
+		static public readonly string DEFAULT_TIME_SEPARATOR = ":";
+	}
+}
