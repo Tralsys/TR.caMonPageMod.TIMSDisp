d6dd86af9f96115dcee731ef5134f55a41f4b7a1
ＴＲ <public@technotter.com>
2020-11-12 01:47:44 +0900

    CollapsedWhenNULLコンバータを追加

--- TR.caMonPageMod.TIMSDisp/_UsefulFuncs/ValueConverters.cs
+++ TR.caMonPageMod.TIMSDisp/_UsefulFuncs/ValueConverters.cs
@@ -119,6 +119,15 @@ namespace TR.caMonPageMod.TIMSDisp._UsefulFuncs
 			=> throw new NotImplementedException();
 		
 	}
+	public class CollapsedWhenNULL : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value == null ? Visibility.Collapsed : Visibility.Visible;
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+		
+	}
 	#endregion
 
 	#region Value Plus/Minus
