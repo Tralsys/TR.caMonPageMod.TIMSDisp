b5d96d1d636ed80ecc72e10c52f951bac6422714
ＴＲ <public@technotter.com>
2020-11-14 02:39:56 +0900

    Update A_5Row.xaml.cs
    Bindingの参照先を, D01AA_Dataクラスに移行

--- TR.caMonPageMod.TIMSDisp/Driver/D01/A_5Row.xaml.cs
+++ TR.caMonPageMod.TIMSDisp/Driver/D01/A_5Row.xaml.cs
@@ -9,42 +9,12 @@ namespace TR.caMonPageMod.TIMSDisp.Driver.D01
 	/// </summary>
 	public partial class A_5Row : Page
 	{
-		public StationData Row0 { get; private set; }
-		= new StationData
-		{
-			StationName = "しけん"
-		};
-		public StationData Row1 { get; private set; }
-		public StationData Row2 { get; private set; }
-		public StationData Row3 { get; private set; }
-		public StationData Row4 { get; private set; }
-		public StationData NextStop { get; private set; }
-		= new StationData()
-		{
-			StationName = "テスト",
-			ArrTime = new TimeData()
-			{
-				HH = 12,
-				IsHHVisible = true,
-				IsMMVisible = false,
-				IsSeparatorVisible = true,
-				IsSSVisible = true,
-				IsVisible = true,
-				MM = 0,
-				Separator = "=",
-				SS = 99
-			},
-			TrackNumber = "N/A",
-			IsTrackNumberVisible = true
-		};
-
-		public bool IsNextStopComing { get; set; } = false;
-
+		D01AA_Data d2show = new D01AA_Data();
 		public A_5Row()
 		{
 			InitializeComponent();
 
-			DataContext = this;
+			DataContext = d2show;
 		}
 	}
 }
