1aed4103976c8b7b1ac845768b7c8d555298b777
ＴＲ <public@technotter.com>
2020-12-06 03:57:03 +0900

    D10AA実装の準備

---
+++ TR.caMonPageMod.TIMSDisp/_CustomControl/D10Ax_ListView1Row.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TR.caMonPageMod.TIMSDisp._CustomControl
+{
+	public class D10Ax_ListView1Row : Control
+	{
+		static D10Ax_ListView1Row() => DefaultStyleKeyProperty.OverrideMetadata(typeof(D10Ax_ListView1Row), new FrameworkPropertyMetadata(typeof(D10Ax_ListView1Row)));
+		
+	}
+}
