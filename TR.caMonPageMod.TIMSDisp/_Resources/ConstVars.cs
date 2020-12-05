f0a04adeb2ebdb6263b615d8e50d699cc0c22f36
ＴＲ <public@technotter.com>
2020-12-06 03:55:32 +0900

    Update ConstVars.cs
    ディレクトリ名等を取得する操作を一元化

--- TR.caMonPageMod.TIMSDisp/_Resources/ConstVars.cs
+++ TR.caMonPageMod.TIMSDisp/_Resources/ConstVars.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Windows.Media;
 
@@ -10,5 +12,8 @@ namespace TR.caMonPageMod.TIMSDisp._Resources
 		static public readonly Brush STATION_DEFAULT_TEXTCOLOR = Brushes.White;
 
 		static public readonly string DEFAULT_TIME_SEPARATOR = ":";
+
+		static public readonly string CURRENT_EXECUTING_ASSEMBLY_FULLPATH = Assembly.GetExecutingAssembly().Location;
+		static public readonly string CURRENT_EXECUTING_ASSEMBLY_DIRECTORY = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 	}
 }
