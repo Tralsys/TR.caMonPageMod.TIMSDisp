a6634d78269fe616884bbf5b8929a375d06254f3
ＴＲ <public@technotter.com>
2020-12-10 02:30:31 +0900

    Update ConstVars.cs
    csvフォーマット対応用定数値を追加

--- TR.caMonPageMod.TIMSDisp/_Resources/ConstVars.cs
+++ TR.caMonPageMod.TIMSDisp/_Resources/ConstVars.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
-using System.Text;
 using System.Windows.Media;
 
 namespace TR.caMonPageMod.TIMSDisp._Resources
@@ -15,5 +12,41 @@ namespace TR.caMonPageMod.TIMSDisp._Resources
 
 		static public readonly string CURRENT_EXECUTING_ASSEMBLY_FULLPATH = Assembly.GetExecutingAssembly().Location;
 		static public readonly string CURRENT_EXECUTING_ASSEMBLY_DIRECTORY = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+		static public readonly string[] CSV_COMMENT_HEADER = new string[] { "//", "#", ";" };
+
+		static public int ToInt(in TRTimeTableIC100 a) => (int)a;
+		static public int ToInt(in TRTimeTable100Station a) => (int)a;
+		public enum TRTimeTableIC100
+		{
+			Identifier,
+			OfficeName,
+			WorkNumber,
+			ReleaseYear,
+			ReleaseMonth,
+			ReleaseDay
+		}
+		public enum TRTimeTable100Station
+		{
+			StaName,
+			Location,
+			RunMM,
+			RunSS,
+			ArrHH,
+			ArrMM,
+			ArrSS,
+			ArrSymbol,
+			IsPass,
+			DepHH,
+			DepMM,
+			DepSS,
+			DepSymbol,
+			TrackName,
+			RuninLim,
+			RunoutLim,
+			StaWork,
+			DispColor,
+
+		}
 	}
 }
